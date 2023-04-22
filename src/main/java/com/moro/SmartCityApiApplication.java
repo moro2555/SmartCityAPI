0deb40cb94d38fa024659a99661aee4c4ae206cc
moro <102158809+moro255@users.noreply.github.com>
2023-04-22 18:30:09 +0800

    准备开始

---
+++ src/main/java/com/moro/SmartCityApiApplication.java
@@ -0,0 +1,13 @@
+package com.moro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCityApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartCityApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moro/SmartCityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCityApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
